51883fc5da5d4727bd6d720b7413b87a8496daf5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-07 17:50:32 +0100

    chore: update page js

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -15,6 +15,26 @@ export default function NewPostPage() {
       imageUrl: "",
     });
 
+    const errors = [];
+
+    if (!title || !!!title.trim().length) {
+      errors.push("please provide a valid title");
+    }
+
+    if (!content || !!!content.trim().length) {
+      errors.push("please provide a valid content");
+    }
+
+    if (!image || !image.size) {
+      errors.push("please provide a valid image");
+    }
+
+    if (!!errors.length) {
+      return {
+        errors,
+      };
+    }
+
     redirect("/feed");
   };
 
